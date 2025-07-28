0a5c4a9b86f46291623fa0874239d08986e3b030
Victor Dmitruk <v.dmitruk.01@mail.ru>
2025-07-29 01:48:02 +0300

    Modifies test_logic

--- notes/tests/test_logic.py
+++ notes/tests/test_logic.py
@@ -16,9 +16,8 @@ User = get_user_model()
 class TestNoteCreation(TestCase):
 
     NOTE_SLUG = 'some_slug'
-    NOTE_TEXT = 'some text'
-    NOTE_TITLE = 'some title'
-    
+    NOTE_TEXT = 'Текст'
+    NOTE_TITLE = 'Заголовок'
 
     @classmethod
     def setUpTestData(cls):
@@ -37,7 +36,7 @@ class TestNoteCreation(TestCase):
         self.client.post(self.url, data=self.form_data)
         notes_count = Note.objects.count()
         self.assertEqual(notes_count, 0)
-    
+
     def test_user_can_create_note(self):
         response = self.auth_client.post(self.url, data=self.form_data)
         self.assertRedirects(response, self.done_url)
@@ -65,3 +64,54 @@ class TestNoteCreation(TestCase):
         )
         notes_count = Note.objects.count()
         self.assertEqual(notes_count, 1)
+
+
+class TestNoteEditDelete(TestCase):
+
+    NOTE_TITLE = 'Заголовок'
+    NOTE_TEXT = 'Текст'
+    NEW_NOTE_TEXT = 'Обновленный текст'
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.author = User.objects.create(username='Автор заметки')
+        cls.author_client = Client()
+        cls.author_client.force_login(cls.author)
+
+        cls.another_user = User.objects.create(username='Другой пользователь')
+        cls.another_user_client = Client()
+        cls.another_user_client.force_login(cls.another_user)
+
+        cls.note = Note.objects.create(
+            title=cls.NOTE_TITLE,
+            text=cls.NOTE_TEXT,
+            author=cls.author
+        )
+
+        cls.edit_url = reverse('notes:edit', args=(cls.note.slug,))
+        cls.delete_url = reverse('notes:delete', args=(cls.note.slug,))
+        cls.done_url = reverse('notes:success')
+        cls.form_data = {
+            'title': cls.NOTE_TITLE,
+            'text': cls.NEW_NOTE_TEXT
+            }
+
+    def test_author_can_delete_note(self):
+        response = self.author_client.delete(self.delete_url)
+        self.assertRedirects(response, self.done_url)
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        note_count = Note.objects.count()
+        self.assertEqual(note_count, 0)
+
+    def test_user_cant_delete_note_of_another_user(self):
+        response = self.another_user_client.delete(self.delete_url)
+        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
+        note_count = Note.objects.count()
+        self.assertEqual(note_count, 1)
+
+    def test_author_can_edit_note(self):
+        response = self.author_client.post(self.edit_url, data=self.form_data)
+        self.assertRedirects(response, self.done_url)
+        self.note.refresh_from_db()
+        self.assertEqual(self.note.text, self.NEW_NOTE_TEXT)
+
