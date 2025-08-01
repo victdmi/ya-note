c744359b28bc298781e937dcc79b85a4ff654f8e
Victor Dmitruk <v.dmitruk.01@mail.ru>
2025-08-02 00:16:55 +0300

    Fixes conftest

--- notes/pytest_tests/conftest.py
+++ notes/pytest_tests/conftest.py
@@ -1,24 +1,23 @@
 import pytest
-
-
 from django.test.client import Client
-
-
 from notes.models import Note
 
 
 @pytest.fixture
 def author(django_user_model):
+    """Фикстура для создания автора."""
     return django_user_model.objects.create(username='Автор')
 
 
 @pytest.fixture
 def not_author(django_user_model):
+    """Фикстура для создания другого пользователя."""
     return django_user_model.objects.create(username='Не автор')
 
 
 @pytest.fixture
 def author_client(author):
+    """Фикстура для создания клиента автора."""
     client = Client()
     client.force_login(author)
     return client
@@ -26,6 +25,7 @@ def author_client(author):
 
 @pytest.fixture
 def not_author_client(not_author):
+    """Фикстура для создания клиента другого пользователя."""
     client = Client()
     client.force_login(not_author)
     return client
@@ -33,6 +33,7 @@ def not_author_client(not_author):
 
 @pytest.fixture
 def note(author):
+    """Фикстура для создания заметки."""
     note = Note.objects.create(
         title='Заголовок',
         text='Текст заметки',
@@ -44,4 +45,5 @@ def note(author):
 
 @pytest.fixture
 def slug_for_args(note):
+    """Фикстура для возврата поля slug."""
     return (note.slug,)
