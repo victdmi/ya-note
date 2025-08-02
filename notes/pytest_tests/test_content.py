e7b91689703a871c99024f4ed93934693c9e5c17
Victor Dmitruk <v.dmitruk.01@mail.ru>
2025-08-02 23:37:00 +0300

    Adds test_content in pytest

---
+++ notes/pytest_tests/test_content.py
@@ -0,0 +1,37 @@
+import pytest
+from django.urls import reverse
+from pytest_lazy_fixtures import lf
+
+from notes.forms import NoteForm
+
+
+@pytest.mark.parametrize(
+    'parametrized_client, note_in_list',
+    (
+        (lf('author_client'), True),
+        (lf('not_author_client'), False)
+    )
+)
+def test_notes_list_for_different_users(
+    note, parametrized_client, note_in_list
+):
+    """Тест отображения заметок для разных пользователей."""
+    url = reverse('notes:list')
+    response = parametrized_client.get(url)
+    object_list = response.context['object_list']
+    assert (note in object_list) is note_in_list
+
+
+@pytest.mark.parametrize(
+    'name, args',
+    (
+        ('notes:add', None),
+        ('notes:edit', lf('slug_for_args'))
+    )
+)
+def test_pages_contain_form(author_client, name, args):
+    """Тест присутствия формы на странице."""
+    url = reverse(name, args=args)
+    response = author_client.get(url)
+    assert 'form' in response.context
+    assert isinstance(response.context['form'], NoteForm)
