aaa9d34ac01a9cf58b7766db89d266f377d14e98
Victor Dmitruk <v.dmitruk.01@mail.ru>
2025-08-03 00:21:40 +0300

    Updates test_content unittest

--- notes/tests/test_content.py
+++ notes/tests/test_content.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
-from django.test import TestCase
+from django.test import Client, TestCase
 from django.urls import reverse
+from notes.forms import NoteForm
 from notes.models import Note
 
 
@@ -8,26 +9,46 @@ User = get_user_model()
 
 
 class TestListPage(TestCase):
-
-    LIST_URL = reverse('notes:list')
+    """Класс для тестирования контента."""
 
     @classmethod
     def setUpTestData(cls):
-        cls.user = User.objects.create(username='Пользователь')
-        Note.objects.bulk_create(
-            Note(
-                title='Заголовок',
-                text='Текст.',
-                slug=f'{index}',
-                author=cls.user
-            )
-            for index in range(2)
+        """Метод, подготавливающий данные для тестов."""
+        cls.author = User.objects.create(username='Автор')
+        cls.not_author = User.objects.create(username='Другой пользователь')
+        cls.note = Note.objects.create(
+            title='Заголовок',
+            text='Текст.',
+            slug='slug',
+            author=cls.author
         )
+        cls.author_client = Client()
+        cls.author_client.force_login(cls.author)
+        cls.not_author_client = Client()
+        cls.not_author_client.force_login(cls.not_author)
 
-    def test_notes_order(self):
-        self.client.force_login(self.user)
-        response = self.client.get(self.LIST_URL)
-        object_list = response.context['object_list']
-        all_id = [note.id for note in object_list]
-        sorted_id = sorted(all_id)
-        self.assertEqual(all_id, sorted_id)
+    def test_notes_list_for_different_users(self):
+        """Тест отображения заметок для разных пользователей."""
+        url = reverse('notes:list')
+        user_clients_note_in_list = (
+            (self.author_client, True),
+            (self.not_author_client, False),
+        )
+        for user_client, note_in_list in user_clients_note_in_list:
+            with self.subTest(user_client=user_client):
+                response = user_client.get(url)
+                object_list = response.context['object_list']
+                self.assertEqual((self.note in object_list), note_in_list)
+
+    def test_pages_contain_form(self):
+        """Тест присутствия формы на странице."""
+        names_args = (
+            ('notes:add', None),
+            ('notes:edit', (self.note.slug,))
+        )
+        for name, args in names_args:
+            with self.subTest(name=name):
+                url = reverse(name, args=args)
+                response = self.author_client.get(url)
+                self.assertIn('form', response.context)
+                self.assertIsInstance(response.context['form'], NoteForm)
